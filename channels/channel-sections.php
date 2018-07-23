530f04871ba0c93a07d28022cee698387a98f47c
shahzadsalamat <shahzad.salamat@yahoo.com>
2018-07-23 18:12:41 +0300

    Adding channels files

---
+++ channels/channel-sections.php
@@ -0,0 +1,90 @@
+ <div class="channels-wrap">
+       		 <ul class="channels">
+                     <li class="channel-image channel-image-fashion">
+                  		<div class="channel-info">
+                        		<p class="channel-title">Fashion</p>
+                                <div class="button1-wrap">
+                               <input type='button' value='following'>
+                        </div>
+                        </div>
+                        
+                        <div class="channel-follower">
+                        	<div class="channel-follower-title"><p> followers</p></div>
+                           <div class="channel-follower-avatar"><?php include("followers.html"); ?></div>
+                        </div>
+                    </li>
+                    
+                    <li class="channel-image channel-image-science">
+                  		<div class="channel-info">
+                        		<p class="channel-title">Science</p>
+                                <div class="button1-wrap">
+                               <input type='button' value='following'>
+                        </div>
+                        </div>
+                        
+                        <div class="channel-follower">
+                        	<div class="channel-follower-title"><p> followers</p></div>
+                           <div class="channel-follower-avatar"><?php include("followers.html"); ?></div>
+                        </div>
+                    </li>
+                    
+                    <li class="channel-image channel-image-auto">
+                  		<div class="channel-info">
+                        		<p class="channel-title">Auto</p>
+                                <div class="button1-wrap">
+                               <input type='button' value='following'>
+                        </div>
+                        </div>
+                        
+                        <div class="channel-follower">
+                        	<div class="channel-follower-title"><p> followers</p></div>
+                           <div class="channel-follower-avatar"><?php include("followers.html"); ?></div>
+                        </div>
+                    </li>
+                    
+                    <li class="channel-image channel-image-technology">
+                  		<div class="channel-info">
+                        		<p class="channel-title">Technology</p>
+                                <div class="button1-wrap">
+                               <input type='button' value='following'>
+                        </div>
+                        </div>
+                        
+                        <div class="channel-follower">
+                        	<div class="channel-follower-title"><p> followers</p></div>
+                           <div class="channel-follower-avatar"><?php include("followers.html"); ?></div>
+                        </div>
+                    </li>
+                    
+                    <li class="channel-image channel-image-finance">
+                  		<div class="channel-info">
+                        		<p class="channel-title">Finance</p>
+                                <div class="button1-wrap">
+                               <input type='button' value='following'>
+                        </div>
+                        </div>
+                        
+                        <div class="channel-follower">
+                        	<div class="channel-follower-title"><p> followers</p></div>
+                           <div class="channel-follower-avatar"><?php include("followers.html"); ?></div>
+                        </div>
+                    </li>
+                    
+                    <li class="channel-image channel-image-environment">
+                  		<div class="channel-info">
+                        		<p class="channel-title">Environment</p>
+                                <div class="button1-wrap">
+                               <input type='button' value='following'>
+                        </div>
+                        </div>
+                        
+                        <div class="channel-follower">
+                        	<div class="channel-follower-title"><p> followers</p></div>
+                           <div class="channel-follower-avatar"><?php include("followers.html"); ?></div>
+                        </div>
+                    </li>
+                    
+                     
+              		
+          </ul>
+     </div>
